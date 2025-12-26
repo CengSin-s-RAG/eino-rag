e375fa13a5f46c99a877d5243015db9019823a6d
zhangzetao <zhangzetao@wallstreetcn.com>
2025-12-26 11:34:32 +0800

    修改context上下文

--- api/eino_handler.go
+++ api/eino_handler.go
@@ -38,13 +38,11 @@ func (h *EinoChatAgentHandler) HandleQuery(c echo.Context) error {
 	}
 
 	store := memory.NewRedisStore(client.Redis)
-
 	var messages []*schema.Message
 	messages, err := store.GetRecentMessages(ctx, input.SessionId, ContextWindowSize)
 	if err != nil {
 		return err
 	}
-
 	userMessage := schema.UserMessage(input.Query)
 	messages = append(messages, userMessage)
 
@@ -53,17 +51,10 @@ func (h *EinoChatAgentHandler) HandleQuery(c echo.Context) error {
 		return err
 	}
 
-	// 【改造点 3】: 异步生成摘要 (可选)
-	// 如果 recentMessages 长度达到阈值，触发一个 goroutine 去压缩早期的历史记录并存入 "summary" 字段
-	// go h.triggerSummarization(input.SessionId)
-
-	if err = store.AddMessage(ctx, input.SessionId, userMessage); err != nil {
-		return err
-	}
-
-	if err = store.AddMessage(ctx, input.SessionId, message); err != nil {
-		return err
-	}
+	go func() {
+		store.AddMessage(ctx, input.SessionId, userMessage)
+		store.AddMessage(ctx, input.SessionId, message)
+	}()
 
 	return c.JSON(http.StatusOK, &ChatResp{SessionID: input.SessionId, ReplyMessage: message.Content})
 }
